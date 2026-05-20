5e3fbbbdc3486806cb486b92bcf9d63934696552
welderbm <welderb40@gmail.com>
2026-05-20 16:35:33 -0400

    Implementa lógica de cálculo de multas por excesso de velocidade no Desafio 03

--- desafios/desafio-03/solucao.py
+++ desafios/desafio-03/solucao.py
@@ -3,3 +3,11 @@
 # Data:  (data de entrega)
 
 # ── Escreva sua solução abaixo ──────────────────────────────────────────────
+velocidade = input("Qual é a velocidade do seu carro em km/h?")
+
+if velocidade > 80 :
+    print("Multado! Você excedeu o limite de 80km/h")
+    excedente = velocidade - 80
+    multa = excedente * 7
+else:
+    print("Boa viagem! Dirija com segurança")
