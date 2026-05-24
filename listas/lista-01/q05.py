974bd52ee602b61fc2946a0e6dc60780a05ee269
WelderBm <welderb40@gmail.com>
2026-05-24 13:13:30 -0400

    atualiza informações do aluno e data na solução do Desafio 05

--- listas/lista-01/q05.py
+++ listas/lista-01/q05.py
@@ -1,6 +1,6 @@
 # Lista 01 — Questão 05: Encontre o Bug
-# Aluno: (seu nome)
-# Data:  (data)
+# Aluno: Welder Barroso de Melo
+# Data:  24-05-2026
 
 # ── Enunciado ───────────────────────────────────────────────────────────────
 # O código abaixo tem um bug lógico — executa sem erros, mas produz resultado errado.
@@ -15,3 +15,15 @@
 #   print(maior_nota([7.5, 9.0, 6.0, 8.5]))
 
 # ── Sua solução abaixo ──────────────────────────────────────────────────────
+
+def maior_nota(notas):
+    maior = 0
+    for nota in notas:
+        if nota > maior:
+            maior = nota
+    return maior
+
+print(maior_nota([7.5, 9.0, 6.0, 8.5]))
+
+# o bug era que tava com dois sinais de igual entao ele tava comparando em vez de salvar o valor da nota na variavel maior
+
